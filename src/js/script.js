29eb935e04df616b8b16133ecb76bb242f6ddb9c
Ethan-Schwartzentruber <ethan.schwartzentruber@edu.univ-fcomte.fr>
2024-12-03 15:06:34 +0100

    essai n°1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 // *** LIGHTBOX ***
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("ul li img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImage = lightbox.querySelector("img");
+
+    // Ouvre la LightBox
+    images.forEach((img) => {
+        img.addEventListener("click", () => {
+            lightboxImage.src = img.getAttribute("data-full-img");
+            lightbox.showModal();
+        });
+    });
+
+    // Ferme la LightBox en cliquant à l'extérieur de l'image
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox || e.target.tagName !== "IMG") {
+            lightbox.close();
+        }
+    });
+});
